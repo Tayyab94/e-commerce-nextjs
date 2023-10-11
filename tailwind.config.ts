71eb68d0bc177847ad37c56e90cc155945752d34
Tayyab94 <tayyab.bhatti30@gmail.com>
2023-10-12 01:42:39 +0500

    Initial Commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,35 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  // theme: {
+  //   extend: {
+  //     backgroundImage: {
+  //       'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+  //       'gradient-conic':
+  //         'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  //     },
+  //   },
+  // },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#f29fdd",
+          "secondary": "#f7bed2",
+          "accent": "#5af418",
+          "neutral": "#211f32",
+          "base-100": "#e7e6ef",
+          "info": "#90beea",
+          "success": "#2ab785",
+          "warning": "#f5c024",
+          "error": "#f8125a",
+          "body": {
+            "background-color": "#e3e6e6"
+          }
+        },
       },
-    },
+    ],
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
 export default config
